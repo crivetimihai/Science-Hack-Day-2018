5f71c4044651a8da0d1f07c1c095283675e03e6a
Criveti Mihai <crivetimihai@gmail.com>
2018-04-21 16:17:44 +0100

    api mock

---
+++ pihack/src/pihack/image.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Displays Raspberry PI sensor data and platform information as a REST API.
+"""
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/api/pi')
+def api_pi():
+    pi = pimock.get_pi_mock()
+    response = app.response_class(
+        response = pi,
+        status = 200,
+        mimetype = 'application/json'
+    )
+    return response
+
+if __name__ == '__main__':
+        app.run('0.0.0.0', port=5000)
